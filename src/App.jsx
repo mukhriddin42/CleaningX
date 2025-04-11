699c693ccb67386333d692de55b769e97f2bf54c
mukhriddin03 <imuhriddin700@gmail.com>
2025-04-11 18:36:00 +0500

    first com

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./assets/components/header/Header"
+import Home from "./assets/components/home/Home"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  )
+}
+
+export default App
