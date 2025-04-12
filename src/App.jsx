906414fc332cc050d902f248e13f74883ad74b4e
mukhriddin03 <imuhriddin700@gmail.com>
2025-04-12 21:36:07 +0500

    togrilandi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import About from "./components/about/about"
+import About from "./components/about/About"
 import Articles from "./components/articles/Articles"
 import Contactus from "./components/contactus/Contactus"
 import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
-import Sanitization from "./components/sanitization/sanitization"
+import Sanitization from "./components/sanitization/Sanitization"
 import Services from "./components/services/Services"
 
 function App() {
